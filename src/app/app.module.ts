1f9183004b5a359406fb2e0578b47c01f6bb3195
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-07-21 16:22:11 +0530

    auth service created and injected in appmodule and other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 // Angular material import
 import { MatButtonModule } from '@angular/material';
 import { MatMenuModule } from '@angular/material/menu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon'
 import { MatCardModule } from '@angular/material/card';
-import { BlogsComponent } from './blogs/blogs.component';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { ProfileComponent } from './profile/profile.component';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatChipsModule} from '@angular/material/chips';
-import { FooterComponent } from './footer/footer.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatChipsModule } from '@angular/material/chips';
+
+
+/* Components */
 import { LoginPageComponent } from './login-page/login-page.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+/* Services Import */
+//Authservice import
+import { AuthService } from './auth.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,10 +47,11 @@ import { LoginPageComponent } from './login-page/login-page.component';
     MatCheckboxModule,
     MatToolbarModule,
     MatPaginatorModule,
-    MatChipsModule
+    MatChipsModule,
+    HttpClientModule
 
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
