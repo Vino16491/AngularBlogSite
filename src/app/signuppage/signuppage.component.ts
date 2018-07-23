df36131d142c54f9903dc8e7949575f8add0184c
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-07-23 21:43:23 +0530

    sign up form input place updated

--- src/app/signuppage/signuppage.component.ts
+++ src/app/signuppage/signuppage.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { userModel } from '../models/userModel';
 @Component({
   selector: 'app-signuppage',
   templateUrl: './signuppage.component.html',
   styleUrls: ['./signuppage.component.css']
 })
 export class SignuppageComponent implements OnInit {
-
+  validFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  userSignUpData = new userModel('', '');
   constructor() { }
 
   ngOnInit() {
+  }
+  onSubmit(){
+    alert('Hello World')
   }
 
 }
