88c0d13cc15c049d50abb4da82ede0fc405a8d3c
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-07-26 21:41:10 +0530

    service created and updated in add story component

--- src/app/add-story/add-story.component.ts
+++ src/app/add-story/add-story.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {BlogDataService} from '../services/blog-data.service'
 @Component({
   selector: 'app-add-story',
   templateUrl: './add-story.component.html',
   styleUrls: ['./add-story.component.css']
 })
 export class AddStoryComponent implements OnInit {
-
-  constructor() { }
+public userStory:{};
+  constructor(public blogService: BlogDataService) { }
 
   ngOnInit() {
+  }
+
+  saveStory(){
+    this.blogService.addUserStory(this.userStory)
   }
 
 }
---
+++ src/app/services/blog-data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogDataService {
+public blogStory;
+  constructor() { }
+
+
+  addUserStory(blogstory){
+    this.blogStory.push(blogstory);
+  }
+}
+
+
