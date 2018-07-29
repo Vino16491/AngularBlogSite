81658b8be9725128cc846d5f1600d665e7aa5e58
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-07-29 21:27:19 +0530

    small bug fixed, and integrated bootstrap links

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -1,33 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
-import {BlogDataService} from '../services/blog-data.service'
+import { BlogDataService } from '../services/blog-data.service'
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
   styleUrls: ['./blogs.component.css']
 })
 export class BlogsComponent implements OnInit {
-blogs = []
+  blogs = []
   constructor(private _authservice: AuthService, public blogdata: BlogDataService) { }
 
   ngOnInit() {
     this.blogsData();
-    
+
   }
 
-  blogsData(){
+  blogsData() {
 
     let storyFromService = this.blogdata.userStory();
     // alert(storyFromService);
-    
-    if(storyFromService){
+
+    if (storyFromService) {
       // for(let story in storyFromService){
-        this.blogs.push(storyFromService);
-        console.log(JSON.stringify(storyFromService));
+      this.blogs.push(storyFromService);
+      console.log(JSON.stringify(storyFromService));
       // }
       // this.blogs.push(storyFromService);
-      
-    }else{
+
+    } else {
       console.log(storyFromService + 'no data found')
     }
   }
