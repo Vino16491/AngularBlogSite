b501d2d18414cd45c074c5d2372f63ad9ecddefe
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-07-30 22:13:24 +0530

    responsive qureries written for navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,11 +7,21 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  
+  showSideNavMenu;
   constructor(public _authservice: AuthService) { }
 
   ngOnInit() {
 
   }
 
+  openSideNav(){
+    if(this.showSideNavMenu === '0px'){
+      this.showSideNavMenu = '250 px';
+    }
+  }
+  closeSideNav(){
+    
+      this.showSideNavMenu = '0 px';
+    
+  }
 }
