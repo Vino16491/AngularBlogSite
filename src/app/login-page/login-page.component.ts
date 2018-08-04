e06e50da69851a41bbec0574851122c4d865bd8a
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-04 21:50:16 +0530

    prettified page

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,42 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
-import { FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { userModel } from '../models/userModel';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth.service";
+import { FormControl, Validators, FormGroup } from "@angular/forms";
+import { Router } from "@angular/router";
+import { userModel } from "../models/userModel";
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  selector: "app-login-page",
+  templateUrl: "./login-page.component.html",
+  styleUrls: ["./login-page.component.css"]
 })
 export class LoginPageComponent implements OnInit {
-
-  validFormControl = new FormControl('', [
+  validFormControl = new FormControl("", [
     Validators.required,
-    Validators.email,
+    Validators.email
   ]);
 
-  userLoginData = new userModel('', '');
-  constructor(private _authservice: AuthService, private router: Router) { }
+  userLoginData = new userModel("", "");
+  constructor(private _authservice: AuthService, private router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   onSubmit() {
     let user = this._authservice.getAuthData();
 
-    for (let i=0; i< user.length; i++){
-      if(user[i].userId == this.userLoginData.email){
-        if(user[i].password == this.userLoginData.password){
+    for (let i = 0; i < user.length; i++) {
+      if (user[i].userId == this.userLoginData.email) {
+        if (user[i].password == this.userLoginData.password) {
           this._authservice.isUserLoggedIn = true;
-          this.router.navigate(['blogs']);
-        }else{
+          this.router.navigate(["blogs"]);
+        } else {
           alert("wrong user id and password");
         }
       }
     }
-    
-
-
-
   }
-
 }
