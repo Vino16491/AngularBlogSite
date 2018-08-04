0352a127218d6b51fbfbcf47db84f67f9c5c0a99
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-04 22:00:55 +0530

    sign update wired with auth service

--- src/app/signuppage/signuppage.component.ts
+++ src/app/signuppage/signuppage.component.ts
@@ -1,26 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { userSignUpModel } from '../models/userModel';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { userSignUpModel } from "../models/userModel";
+import { AuthService } from "../auth/auth.service";
 @Component({
-  selector: 'app-signuppage',
-  templateUrl: './signuppage.component.html',
-  styleUrls: ['./signuppage.component.css']
+  selector: "app-signuppage",
+  templateUrl: "./signuppage.component.html",
+  styleUrls: ["./signuppage.component.css"]
 })
 export class SignuppageComponent implements OnInit {
-  validFormControl = new FormControl('', [
+  validFormControl = new FormControl("", [
     Validators.required,
-    Validators.email,
+    Validators.email
   ]);
-  userSignUpData = new userSignUpModel('', '', '', null);
-  constructor(private router: Router) { }
+  userSignUpData = new userSignUpModel("", "", "", null);
+  constructor(private router: Router, private authService: AuthService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  onSubmit() {
+    this.authService.registeredUser({
+      email: this.userSignUpData.email,
+      password: this.userSignUpData.password
+    });
   }
-  onSubmit(){
-
-    // alert('Hello World ' + JSON.stringify(this.userSignUpData));
-    this.router.navigate(['blogs']);
-  }
-
 }
