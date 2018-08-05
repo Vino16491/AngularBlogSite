222f1e1fce67038f98a7be087a6d6694a62cf6c8
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-08-05 21:01:22 +0530

    forms stated properly

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -2,31 +2,32 @@ import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../auth/auth.service";
 import { FormControl, Validators, FormGroup } from "@angular/forms";
 import { Router } from "@angular/router";
-import { userModel } from "../models/userModel";
 @Component({
   selector: "app-login-page",
   templateUrl: "./login-page.component.html",
   styleUrls: ["./login-page.component.css"]
 })
 export class LoginPageComponent implements OnInit {
-  validFormControl = new FormControl("", [
-    Validators.required,
-    Validators.email
-  ]);
+  loginForm: FormGroup;
 
-  userLoginData = new userModel("", "");
   constructor(private _authservice: AuthService, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      emailId: new FormControl("", {
+        validators: [Validators.required, Validators.email]
+      }),
+      userPass: new FormControl("", { validators: [Validators.required] })
+    });
+  }
   onSubmit() {
-
     this._authservice.login({
-      email: this.userLoginData.email,
-      password: this.userLoginData.password
+      email: this.loginForm.value.emailId,
+      password: this.loginForm.value.userPass
     });
 
-    if(this._authservice.isAuth()){
-      this.router.navigate(['/blogs']);
+    if (this._authservice.isAuth()) {
+      this.router.navigate(["/blogs"]);
     }
   }
 }
--- src/app/signuppage/signuppage.component.ts
+++ src/app/signuppage/signuppage.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { FormControl, Validators } from "@angular/forms";
+import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
-import { userSignUpModel } from "../models/userModel";
 import { AuthService } from "../auth/auth.service";
 @Component({
   selector: "app-signuppage",
@@ -9,18 +8,13 @@ import { AuthService } from "../auth/auth.service";
   styleUrls: ["./signuppage.component.css"]
 })
 export class SignuppageComponent implements OnInit {
-  validFormControl = new FormControl("", [
-    Validators.required,
-    Validators.email
-  ]);
-  userSignUpData = new userSignUpModel("", "", "", null);
   constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {}
-  onSubmit() {
+  onSubmit(form: NgForm) {
     this.authService.registeredUser({
-      email: this.userSignUpData.email,
-      password: this.userSignUpData.password
+      email: form.value.emailId,
+      password: form.value.userPass
     });
 
     if (this.authService.isAuth) {
