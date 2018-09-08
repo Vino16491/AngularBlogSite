58599a7335cd4fd41823ac61e1456c769e33c6cd
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-08 22:57:15 +0530

    created some basic api and added some packages

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,41 @@
 import * as functions from 'firebase-functions';
+/** module for password encryption and decryption */
+import bcrypt = require("bcryptjs");
+/** module for creating apis  */
+import express = require("express");
+/** module for parsing data from documents  */
+import bodyParser = require("body-parser");
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.get('/blog',(req, res)=>{
+    res.status(201).json({message:'getBlogs'})
+})
+app.post('/addstory',(req, res)=>{res.status(201).json({message:'addStory'})})
+app.put('/updateblog',(req,res)=>{res.status(201).json({message:'updateBlogs'})})
+app.delete('/delete',(req,res)=>{res.status(201).json({message:'delteBlogs'})})
+
+
+
+
+
+exports.blogapi = functions.https.onRequest(app)
