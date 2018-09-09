e962850db2ea4f1b43d4785913602a53683f6285
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-09 21:20:06 +0530

    database connected Successfully, error with body-parse

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,22 @@ import express = require("express");
 /** module for parsing data from documents  */
 import bodyParser = require("body-parser");
 
+/* dbconnect */
+import { dbConnect, dbConnectLocal } from "./dbconnect";
+import mongoose = require("mongoose");
+const blog = require("./dbconnect");
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  dbConnect,
+  { useNewUrlParser: true },
+  err => {
+    if (err) {
+      return console.log("err in connection " + err);
+    }
+    return console.log("db connected successfuly");
+  }
+);
+
 const app = express();
 app.use(bodyParser.json());
 app.use(
@@ -26,17 +42,69 @@ app.use(function(req, res, next) {
   next();
 });
 
+/** Get Blogs  */
 app.get("/blog", (req, res) => {
-  res.status(201).json({ message: "getBlogs" });
+  blog.find({}).exec((err, blogs) => {
+    if (err) {
+      return res.status(500).json({ message: err });
+    }
+    return res.status(202).json({ message: "get blog success", blog: blogs });
+  });
 });
+
+/** Add a new story  */
 app.post("/addstory", (req, res) => {
-  res.status(201).json({ message: "addStory" });
+  let newStory = new blog();
+  newStory.title = req.body.title;
+  newStory.story = req.body.title;
+  newStory.imageUrl = req.body.imageUrl;
+
+  newStory.save((err, insertStory) => {
+    if (err) {
+      return res.status(500).json({ message: err });
+    }
+
+    return res
+      .status(201)
+      .json({ message: "add blog success", blog: insertStory });
+  });
 });
-app.put("/updateblog", (req, res) => {
-  res.status(201).json({ message: "updateBlogs" });
+
+/** Update Story  */
+app.put("/updateblog/:id", (req, res) => {
+  blog.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        title: req.body.title,
+        imageUrl: req.body.imageUrl,
+        story: req.body.story
+      }
+    },
+    {
+      new: true
+    },
+    (err, updateBlog) => {
+      if (err) {
+        return res.status(500).json({ message: err });
+      }
+      return res
+        .status(202)
+        .json({ message: "update blog success", blog: updateBlog });
+    }
+  );
 });
-app.delete("/delete", (req, res) => {
-  res.status(201).json({ message: "delteBlogs" });
+
+app.delete("/delete/:id", (req, res) => {
+  blog.findByIdAndRemove(req.body.id, (err, delBlogs) => {
+    if (err) {
+      return res.status(500).json({ message: err });
+    }
+
+    return res
+      .status(202)
+      .json({ message: "update blog success", blog: delBlogs });
+  });
 });
 
 exports.blogapi = functions.https.onRequest(app);
