a2e834d018b3591f1744a5b8eb9a3c72729139f5
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-25 22:20:44 +0530

    trying sticky element but it does'nt works

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -7,48 +7,16 @@ import { BlogDataService } from "../services/blog-data.service";
   styleUrls: ["./blogs.component.css"]
 })
 export class BlogsComponent implements OnInit {
-  blogs = [
-    {
-      title: "Hero 1",
-      story: ""
-    },
-    {
-      title: "Hero 2",
-      story: ""
-    },
-    {
-      title: "Hero 3",
-      story: ""
-    },
-    {
-      title: "Hero 4",
-      story: ""
-    },
-    {
-      title: "Hero 5",
-      story: ""
-    },
-    {
-      title: "Hero 6",
-      story: ""
-    }
-  ];
+  
   constructor(
     private _authservice: AuthService,
     public blogdata: BlogDataService
   ) {}
-
+  
   ngOnInit() {
     this.blogsData();
   }
 
   blogsData() {
-    let storyFromService = this.blogdata.userStory();
-    if (storyFromService) {
-      this.blogs.push(storyFromService);
-      console.log(JSON.stringify(storyFromService));
-    } else {
-      console.log(storyFromService + "no data found");
-    }
   }
 }
