049ccf068320cafe54e70f76979ea463e94ffe25
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-27 20:53:26 +0530

    css and align ment updated for text editor

--- src/app/add-story/add-story.component.ts
+++ src/app/add-story/add-story.component.ts
@@ -1,26 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { BlogDataService } from '../services/blog-data.service';
-import { AngularEditorConfig } from '@kolkov/angular-editor';
-import { Router } from '../../../node_modules/@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { BlogDataService } from "../services/blog-data.service";
+import { AngularEditorConfig } from "@kolkov/angular-editor";
+import { Router } from "../../../node_modules/@angular/router";
 
 @Component({
-  selector: 'app-add-story',
-  templateUrl: './add-story.component.html',
-  styleUrls: ['./add-story.component.css']
+  selector: "app-add-story",
+  templateUrl: "./add-story.component.html",
+  styleUrls: ["./add-story.component.css"]
 })
 export class AddStoryComponent implements OnInit {
   public userStory;
   public userStoryTitle;
   editorConfig: AngularEditorConfig = {
-    defaultFontName:'Roboto',
+    defaultFontName: "Roboto",
     // showToolbar:false,
     editable: true,
     spellcheck: true,
-    height: '10rem',
-    minHeight: '60vh',
-    placeholder: 'Your Story Here...',
-    translate: 'no',
-    uploadUrl: 'v1/images', // if needed
+    height: "10rem",
+    minHeight: "60vh",
+    placeholder: "Your Story Here...",
+    translate: "no",
+    defaultFontSize: "6",
+    enableToolbar: true,
+    showToolbar: false,
+    // uploadUrl: 'v1/images', // if needed
     customClasses: [ // optional
       {
         name: "quote",
@@ -43,34 +46,43 @@ export class AddStoryComponent implements OnInit {
   // myGroup = new FormGroup(){
 
   // }
-  constructor(public blogService: BlogDataService, public router: Router) { }
+  constructor(public blogService: BlogDataService, public router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   saveStory() {
-    if (this.userStoryTitle){
-      if (this.userStory !== undefined && this.userStory !== null && this.userStory) {
+    if (this.userStoryTitle) {
+      if (
+        this.userStory !== undefined &&
+        this.userStory !== null &&
+        this.userStory
+      ) {
         if (this.userStory.length > 50) {
           let userFullStory = {
-            "title": this.userStoryTitle,
-            "story": this.userStory
-          }
+            title: this.userStoryTitle,
+            story: this.userStory
+          };
           this.blogService.addUserStory(userFullStory);
           this.userStory = null;
           this.userStoryTitle = null;
-          this.router.navigate(['blogs']);
-
+          this.router.navigate(["blogs"]);
         } else {
-          alert("story should be more than 50 char" + "Your story is only " + this.userStory.length + ' char')
+          alert(
+            "story should be more than 50 char" +
+              "Your story is only " +
+              this.userStory.length +
+              " char"
+          );
         }
       }
-    }else if (this.userStoryTitle == null || this.userStoryTitle){
+    } else if (this.userStoryTitle == null || this.userStoryTitle) {
       alert("Please provide some title to your story");
     }
-      
-
-
   }
+
+  showToolbar() {
+    this.editorConfig.showToolbar = true;
+  }
+
 
 }
