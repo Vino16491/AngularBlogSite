9f0fe48e095b5e1b74b29edb98d97e8542283c59
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-29 22:27:02 +0530

    basic noty test done success

--- src/app/blogs/add-story/add-story.component.ts
+++ src/app/blogs/add-story/add-story.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { BlogDataService } from "../services/blog-data.service";
 import { AngularEditorConfig } from "@kolkov/angular-editor";
 import { Router } from "@angular/router";
-
+import * as Noty from "noty";
 @Component({
   selector: "app-add-story",
   templateUrl: "./add-story.component.html",
@@ -48,34 +48,36 @@ export class AddStoryComponent implements OnInit {
   ngOnInit() {}
 
   saveStory() {
-    if (this.userStoryTitle) {
-      if (
-        this.userStory !== undefined &&
-        this.userStory !== null &&
-        this.userStory
-      ) {
-        if (this.userStory.length > 50) {
-          let userFullStory = {
-            title: this.userStoryTitle,
-            story: this.userStory,
-            author: this.author?this.author : 'Anonymous',
-          };
-          this.blogService.addUserStory(userFullStory);
-          this.userStory = null;
-          this.userStoryTitle = null;
-          this.router.navigate(["blogs"]);
-        } else {
-          alert(
-            "story should be more than 50 char" +
-              "Your story is only " +
-              this.userStory.length +
-              " char"
-          );
-        }
-      }
-    } else if (this.userStoryTitle == null || this.userStoryTitle) {
-      alert("Please provide some title to your story");
-    }
+    let noty = new Noty({ text: "noty test" })
+    noty.show();
+    // if (this.userStoryTitle) {
+    //   if (
+    //     this.userStory !== undefined &&
+    //     this.userStory !== null &&
+    //     this.userStory
+    //   ) {
+    //     if (this.userStory.length > 50) {
+    //       let userFullStory = {
+    //         title: this.userStoryTitle,
+    //         story: this.userStory,
+    //         author: this.author ? this.author : "Anonymous"
+    //       };
+    //       this.blogService.addUserStory(userFullStory);
+    //       this.userStory = null;
+    //       this.userStoryTitle = null;
+    //       this.router.navigate(["blogs"]);
+    //     } else {
+    //       alert(
+    //         "story should be more than 50 char" +
+    //           "Your story is only " +
+    //           this.userStory.length +
+    //           " char"
+    //       );
+    //     }
+    //   }
+    // } else if (this.userStoryTitle == null || this.userStoryTitle) {
+    //   alert("Please provide some title to your story");
+    // }
   }
 
   showToolbar() {
