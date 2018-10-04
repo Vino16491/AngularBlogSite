3d404beb2eef3a860e5bd1060d6925852387fb68
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-04 05:50:02 +0530

    displayed data on blog card, need little formatting

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../auth/auth.service";
 import { BlogDataService } from "./services/blog-data.service";
 import { Router } from "@angular/router";
+export interface Idata {
+  message: "";
+  blog: [];
+}
 @Component({
   selector: "app-blogs",
   templateUrl: "./blogs.component.html",
@@ -12,20 +16,21 @@ export class BlogsComponent implements OnInit {
   constructor(
     private _authservice: AuthService,
     public blogdata: BlogDataService,
-    private router: Router,
+    private router: Router
   ) {}
-  
+
   ngOnInit() {
     this.blogsData();
   }
 
   blogsData() {
-    this.blogdata.userStory().subscribe(blogs=>this.blogs = blogs);
-    console.log(this.blogs);
+    this.blogdata.userStory().subscribe((blog: Idata) => {
+      return (this.blogs = blog.blog);
+    });
+    // console.log(this.blogs);
   }
 
-  readBlog(){
-    this.router.navigate(['readblog']);
+  readBlog() {
+    this.router.navigate(["readblog"]);
   }
-
 }
