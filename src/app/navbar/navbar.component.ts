34b8d13db784eb985f49a7b9fc46721580003a0d
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-07 01:33:19 +0530

    need to create store for blog

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,6 +5,7 @@ import { Subscription, Observable } from "rxjs";
 /* NgRx import */
 import { Store } from "@ngrx/store";
 import * as fromRoot from "../app.reducer";
+import { BlogDataService } from "../blogs/services/blog-data.service";
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
@@ -16,10 +17,11 @@ export class NavbarComponent implements OnInit, OnDestroy {
   
   isAuth$ : Observable<boolean>;
 
-  constructor(private store: Store<fromRoot.State>, private authService:AuthService) {}
+  constructor(private store: Store<fromRoot.State>, private authService:AuthService, private bserv: BlogDataService) {}
 
   ngOnInit() {
    this.isAuth$ = this.store.select(fromRoot.getIsAuth);
+   
   }
   onToggleSideNav() {
     this.sidenavToggle.emit();
