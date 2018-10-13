5e54721a7b771409abf9c03fc11b77a460479447
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-13 21:58:27 +0530

    new password form

--- src/app/auth/new-password/new-password.component.ts
+++ src/app/auth/new-password/new-password.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-new-password',
   templateUrl: './new-password.component.html',
   styleUrls: ['./new-password.component.css']
 })
 export class NewPasswordComponent implements OnInit {
-
+  newPasswordForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.newPasswordForm = new FormGroup({
+      newPassword: new FormControl("", {
+        validators: [Validators.required, Validators.email]
+      }),
+
+      confirmPassword:new FormControl("", {
+        validators: [Validators.required, Validators.email]
+      }),
+    
+    });
   }
 
 }
