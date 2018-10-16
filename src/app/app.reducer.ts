85d1f56ebc136d13515cfdd9aeef1d4a3123f2f0
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-16 19:49:02 +0530

    app.reducer.ts              --->> integrated ui loader login-page.component.html   --->> progress spinner added login-page.component.ts     --->> store module integrated navbar.component.html       --->> position value updated for mat menu ui.action.ts                --->> actions updated for UI ui.reducer.ts               --->> actions updated for UI reducer

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -5,16 +5,19 @@ import {
 } from "@ngrx/store";
 
 import * as fromAuth from "./auth/auth.reducer";
+import * as fromUi from "./sharedUI/ui.reducer";
 
 /* For blogs */
 import * as fromBlogs from "./blogs/blog.reducer"
  export interface State {
+  ui: fromUi.State;
   auth: fromAuth.State;
   blogs:fromBlogs.State[];
   
 }
 
 export const reducers: ActionReducerMap<State> = {
+  ui: fromUi.uiReducer,
   auth: fromAuth.authReducer,
   blogs:fromBlogs.blogReducer
   
@@ -26,3 +29,7 @@ export const getIsAuth = createSelector(getAuthState, fromAuth.getIsAuth);
 /* For Blogs */
 export const getBlogsState = createFeatureSelector<fromBlogs.State>("blogs");
 export const getBlogs = createSelector(getBlogsState, fromBlogs.getBlogs);
+
+/* For Ui Loading */
+export const getUiState = createFeatureSelector<fromUi.State>("ui");
+export const getIsLoading = createSelector(getUiState, fromUi.getIsLoading);
--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -4,7 +4,9 @@ import { Router } from "@angular/router";
 import { AuthService } from "../auth.service";
 import { MatDialog } from "@angular/material";
 import { PassresetComponent } from "../passreset/passreset.component";
-
+import { Observable } from "rxjs";
+import { Store } from "@ngrx/store";
+import * as fromRoot from "../../app.reducer";
 @Component({
   selector: "app-login-page",
   templateUrl: "./login-page.component.html",
@@ -12,8 +14,13 @@ import { PassresetComponent } from "../passreset/passreset.component";
 })
 export class LoginPageComponent implements OnInit {
   loginForm: FormGroup;
-  loginAttempt= 0;
-  constructor(private _authservice: AuthService, private router: Router, private dialog:MatDialog) {}
+  loginAttempt = 0;
+  constructor(
+    private _authservice: AuthService,
+    private router: Router,
+    private dialog: MatDialog,
+    private store: Store<fromRoot.State>
+  ) {}
 
   ngOnInit() {
     this.loginForm = new FormGroup({
@@ -24,19 +31,19 @@ export class LoginPageComponent implements OnInit {
     });
   }
 
-  forgetPassword():void{
+  forgetPassword(): void {
     const passResetDialog = this.dialog.open(PassresetComponent);
-    passResetDialog.afterClosed().subscribe(result=>{
+    passResetDialog.afterClosed().subscribe(result => {
       console.log(result);
-      this._authservice.passresetMongoServer(result)
-    })
+      this._authservice.passresetMongoServer(result);
+    });
   }
 
   onSubmit() {
-    this.loginAttempt++
+    this.loginAttempt++;
     this._authservice.loginMongoServer({
       email: this.loginForm.value.emailId,
       password: this.loginForm.value.userPass
-    })
+    });
   }
 }
